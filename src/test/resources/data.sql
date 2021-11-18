a8ff8b38112fe5d14c61077ba5c0bd9b146a303a
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-18 17:56:17 +0100

    Added database in memory and dependencies

---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO 
+	CUSTOMER (NAME, SURNAME, PHOTO, CREATION_USER, MODIFICATION_USER) 
+VALUES
+  	('Pepe', 'Pepin', 1, 'creator1', 'modificator1'),
+  	('Paco', 'Paquin', 2, 'creator2', 'modificator2')
+;
