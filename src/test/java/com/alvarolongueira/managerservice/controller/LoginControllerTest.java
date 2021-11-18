9a76eb91e35a069074a8851fd91932d756ae978d
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-18 17:57:29 +0100

    Pretty variable

--- src/test/java/com/alvarolongueira/managerservice/controller/LoginControllerTest.java
+++ src/test/java/com/alvarolongueira/managerservice/controller/LoginControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.alvarolongueira.managerservice.controller.LoginController;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginControllerTest {
 
@@ -24,8 +22,8 @@ public class LoginControllerTest {
 	@Test
 	public void testMainUrl() throws Exception {
 		Assert.assertNotNull(controller);
-		ResponseEntity<String> response = template.getForEntity(MAIN_URL, String.class);
-		Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+		ResponseEntity<String> responseEntity = template.getForEntity(MAIN_URL, String.class);
+		Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
 	}
 
 	@Test
--- src/test/java/com/alvarolongueira/managerservice/controller/UserControllerTest.java
+++ src/test/java/com/alvarolongueira/managerservice/controller/UserControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.alvarolongueira.managerservice.controller.UserController;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
 
@@ -24,8 +22,8 @@ public class UserControllerTest {
 	@Test
 	public void testMainUrl() throws Exception {
 		Assert.assertNotNull(controller);
-		ResponseEntity<String> response = template.getForEntity(MAIN_URL, String.class);
-		Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+		ResponseEntity<String> responseEntity = template.getForEntity(MAIN_URL, String.class);
+		Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
 	}
 
 	@Test
