51a014db1c10b554666a9a0fd341849fac810a08
Alvaro Longueira <alvarolco@ext.inditex.com>
2021-11-24 18:22:11 +0100

    Refactor auditory

--- src/main/java/com/alvarolongueira/managerservice/configuration/AuditorConfig.java
+++ src/main/java/com/alvarolongueira/managerservice/configuration/AuditorConfig.java
@@ -2,14 +2,10 @@ package com.alvarolongueira.managerservice.configuration;
 
 import java.util.Optional;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Configuration
-@EnableJpaAuditing
 public class AuditorConfig implements AuditorAware<String> {
 
 	@Override
